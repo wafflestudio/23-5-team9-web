04360d2b8b5aa701bcd6236a69888aa41ee75fe3
yabsed <yabsed@snu.ac.kr>
2026-01-11 00:38:21 +0900

    varcel

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Vercel 배포 시에는 보통 '/'를 사용합니다.
+  base: '/',
   server: {
     watch: {
       usePolling: true,
     },
+    proxy: {
+      '/api': {
+        target: 'https://dev.server.team9-toy-project.p-e.kr',
+        changeOrigin: true,
+      },
+    },
   },
-  base: '/23-5-team9-web/'
 })
